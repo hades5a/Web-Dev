d7411829fadccab149bcd750c9cf0b841799d5a3
Felix Nguyen <feling289@gmail.com>
2020-03-22 11:31:48 +0100

    First Init

---
+++ js.js
@@ -0,0 +1,21 @@
+if (magic == ds) {
+
+}
+
+var x =
+
+x[0] = 
+
+var y = []
+
+x.push()
+x.splite(x,x,x)
+
+for (var i = 3; i < 7; i++) {
+
+}
+
+// Aufgabe Array mittels schleife wiedergeben
+// dann in p tags
+
+// Aufgabe Schätzspiel
